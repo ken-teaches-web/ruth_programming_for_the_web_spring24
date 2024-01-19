6dac03e0acb8ecf4e2d2fb11e29c4341d0719ee8
ruthbaccam <ruthbaccam4797@gmail.com>
2024-01-18 23:52:16 -0600

    week_1_exercises

---
+++ week_1/exercise_2/sketch.js
@@ -0,0 +1,15 @@
+const scale = window.prompt("How big do you want this circle?");
+
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    if (mouseIsPressed) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+  }
+  
